02f3075978ca263053885966f0119f742d2a3250
24418305 <s0610173@my.cmsh.cyc.edu.tw>
2019-01-29 18:18:39 +0800

    add DoubleSolenoid

--- src/main/java/frc/system/Hatch.java
+++ src/main/java/frc/system/Hatch.java
@@ -2,20 +2,33 @@ package frc.system;
 
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import frc.robot.Robot;
 
 public class Hatch {
     
-   public static Compressor com;
-   public static DoubleSolenoid dou;
-   public static DoubleSolenoid dou1;
+   public static Compressor air;
+   public static DoubleSolenoid dpush;
+   public static DoubleSolenoid dpush2;
   
-   public static void init() {
-    com = new Compressor();
-    com.setClosedLoopControl(true);
+   public static void init() 
+   {
+    air = new Compressor();
+    air.setClosedLoopControl(true);
+    dpush  = new DoubleSolenoid(0, 1);
+    dpush2 = new DoubleSolenoid(2, 3);
    }
 
 
-   public static void controlCompressor(boolean on){
-       com.setClosedLoopControl(on);
+   public static void controlCompressor(boolean on)
+   {
+       air.setClosedLoopControl(on);
+   }
+   public static void tele()
+   {
+    if(Robot.xBox.getAButton());
+    {
+           
+    }
+
    }
 }
