b24a8d1473261dbe61f33f83b073ddacae03017a
kellybalabala <teahouse171@gmail.com>
2019-01-29 17:43:35 +0800

    finished

--- src/main/java/frc/system/Shooting.java
+++ src/main/java/frc/system/Shooting.java
@@ -1,17 +1,36 @@
 package frc.system;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Ultrasonic;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class Shooting{
+public class Shooting {
 
     public static Ultrasonic rangeSensor;
+    public static Encoder enc;
+    public static VictorSP vic;
+    public static int target;
+    public static double kP;
 
-    public static void init(){
+    public static void init() {
         rangeSensor = new Ultrasonic(1, 0);
+        enc = new Encoder(0, 0);
+        vic = new VictorSP(0);
+        SmartDashboard.putNumber("kP", 0);
+        SmartDashboard.putNumber("Target", 0);
     }
 
-    public static double getRange(){
+    public static double getRange() {
         return rangeSensor.getRangeMM();
     }
 
+    public static void teleop() {
+        kP = SmartDashboard.getNumber("kP", 0);
+        target = (int) SmartDashboard.getNumber("Target", 0);
+        int error = enc.get() - target;
+        SmartDashboard.putNumber("error", error);
+        double out = error * kP;
+        vic.set(out);
+    }
 }
