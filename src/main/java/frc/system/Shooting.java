c14ce0a0bc14c67ef50bef1257bdd36626fdaab1
kellybalabala <teahouse171@gmail.com>
2019-01-29 22:34:40 +0800

    add the bottons to shoot

--- src/main/java/frc/system/Shooting.java
+++ src/main/java/frc/system/Shooting.java
@@ -4,12 +4,15 @@ import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Robot;
 
 public class Shooting {
 
     public static Ultrasonic rangeSensor;
     public static Encoder enc;
     public static VictorSP vic;
+    public static VictorSP vic2;
+    public static VictorSP vic3;
     public static int target;
     public static double kP;
 
@@ -17,6 +20,8 @@ public class Shooting {
         rangeSensor = new Ultrasonic(1, 0);
         enc = new Encoder(0, 0);
         vic = new VictorSP(0);
+        vic2 = new VictorSP(2);
+        vic3 = new VictorSP(3);
         SmartDashboard.putNumber("ShootingkP", 0);
         SmartDashboard.putNumber("Target", 0);
     }
@@ -32,5 +37,18 @@ public class Shooting {
         SmartDashboard.putNumber("error", error);
         double out = error * kP;
         vic.set(out);
+
+        if(Robot.xBox.getAButton()){
+            vic2.set(0.6);
+            vic3.set(-0.6);
+        }
+        else if(Robot.xBox.getBButton()){
+            vic2.set(0.3);
+            vic3.set(-0.3);
+        }
+        else{
+            vic2.set(0);
+            vic3.set(0);
+        }
     }
 }
